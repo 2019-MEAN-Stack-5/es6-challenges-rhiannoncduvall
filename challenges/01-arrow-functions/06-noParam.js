99646b2e2cdcd8a6ad877f76500635dfefcacb44
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-09-17 20:48:49 -0700

    06 passed tests

--- challenges/01-arrow-functions/06-noParam.js
+++ challenges/01-arrow-functions/06-noParam.js
@@ -1,13 +1,21 @@
-//rewrite the below function as an arrow funtion. In the funciton declare 2 variables and assign any two strings as their value.
-// Finally, return the two strings concatenated together. Since the funciotn doesn't require any paramaters
-// you should use the empty () syntax of _ syntax.
+//rewrite the below function as an arrow funtion. 
+
+// In the function declare 2 variables and assign any two strings as their value.
+
+// Finally, return the two strings concatenated together. 
+
+// Since the funciotn doesn't require any paramaters you should use the empty () syntax of _ syntax.
 
 
-function noParam() {
+// function noParam() {
 
+// }
+
+const noParam = () => {
+    let var1 = "hello";
+    let var2 = "goodbye";
+    return `You say ${var2} and I say ${var1}.`
 }
-
-
 
 
 
