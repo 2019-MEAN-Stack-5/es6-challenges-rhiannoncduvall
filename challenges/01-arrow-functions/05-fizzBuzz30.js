8c4c02fea62cda59a1fe7a1cd3d65ea1d9a5eb27
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-09-17 20:45:14 -0700

    05 passed tests

--- challenges/01-arrow-functions/05-fizzBuzz30.js
+++ challenges/01-arrow-functions/05-fizzBuzz30.js
@@ -2,14 +2,27 @@
 //1 through 30 replacing numbers divisible by 3 with 'fizz', numbers divisible by 5 with 'buzz' and numbers divisible by 3 and 5 with
 //'fizzbuzz'
 
-let fizzBuzz30;
+// let fizzBuzz30 = () => 
 
 
 
-
-
+let fizzBuzz30 = () => {
+    let output = [];
+    for (let i = 1; i < 31; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            output.push('fizzbuzz')
+        }   else if (i % 3 === 0) {
+            output.push('fizz');
+        } else if (i % 5 === 0) {
+            output.push('buzz');
+        } else {
+            output.push(i);
+        }
+    } return output;
+}
 
 
 
 //Do not edit
 module.exports = fizzBuzz30;
+
